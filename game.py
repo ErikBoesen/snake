33f3a001ffc0a73654038a70c6c26b6aa1a74af3
ErikBoesen <me@erikboesen.com>
2018-12-02 21:29:43 -0500

    Rewrite half of board code

--- game.py
+++ game.py
@@ -14,9 +14,9 @@ class Apple:
     def place(self):
         self.x = random.randint(WIDTH)
         self.y = random.randint(HEIGHT)
-
+"""
     def draw(self, surface, image):
-        pygame.draw.rect(surface, [self.x, self.y, STEP, STEP])
+        pygame.draw.rect(surface, [self.x, self.y, STEP, STEP])"""
 
 
 class Segment:
@@ -55,153 +55,39 @@ class Snake:
                 return True
         return False
 
+    # TODO: Implement ate method here?
+    """
     def draw(self, surface, image):
         for i in range(0,self.length):
-            surface.blit(image,(self.x[i],self.y[i]))
+            surface.blit(image,(self.x[i],self.y[i]))"""
 
-
-
-class Computer:
-    x = [0]
-    y = [0]
-    step = 44
-    direction = 0
-    length = 3
-
-    updateCountMax = 2
-    updateCount = 0
-
-    def __init__(self, length):
-       self.length = length
-       for i in range(0,2000):
-           self.x.append(-100)
-           self.y.append(-100)
-
-       # initial positions, no collision.
-       self.x[0] = 1*44
-       self.y[0] = 4*44
-
-    def update(self):
-
-        self.updateCount = self.updateCount + 1
-        if self.updateCount > self.updateCountMax:
-
-            # update previous positions
-            for i in range(self.length-1,0,-1):
-                self.x[i] = self.x[i-1]
-                self.y[i] = self.y[i-1]
-
-            # update position of head of snake
-            if self.direction == 0:
-                self.x[0] = self.x[0] + STEP
-            if self.direction == 1:
-                self.x[0] = self.x[0] - STEP
-            if self.direction == 2:
-                self.y[0] = self.y[0] - STEP
-            if self.direction == 3:
-                self.y[0] = self.y[0] + STEP
-
-            self.updateCount = 0
-
-
-    def moveRight(self):
-        self.direction = 0
-
-    def moveLeft(self):
-        self.direction = 1
-
-    def moveUp(self):
-        self.direction = 2
-
-    def moveDown(self):
-        self.direction = 3
-
-
-    def target(self,dx,dy):
-        if self.x[0] > dx:
-            self.moveLeft()
-
-        if self.x[0] < dx:
-            self.moveRight()
-
-        if self.x[0] == dx:
-            if self.y[0] < dy:
-                self.moveDown()
-
-            if self.y[0] > dy:
-                self.moveUp()
-
-    def draw(self, surface, image):
-        for i in range(0,self.length):
-            surface.blit(image,(self.x[i],self.y[i]))
-
-
-class Game:
-    def isCollision(self,x1,y1,x2,y2,bsize):
-        if x1 >= x2 and x1 <= x2 + bsize:
-            if y1 >= y2 and y1 <= y2 + bsize:
-                return True
-        return False
 
 class App:
-
-    windowWidth = 800
-    windowHeight = 600
-    player = 0
-    apple = 0
+    # TODO: Move actual game logic to Game object
+    snake = Snake(1, 1)
+    apple = Apple()
 
     def __init__(self):
-        self._running = True
-        self._display_surf = None
-        self._image_surf = None
-        self._apple_surf = None
-        self.game = Game()
-        self.player = Player(5)
-        self.apple = Apple(8,5)
-        self.computer = Computer(5)
+        self.running = True
 
     def on_init(self):
         pygame.init()
-        self._display_surf = pygame.display.set_mode((self.windowWidth,self.windowHeight), pygame.HWSURFACE)
-
-        pygame.display.set_caption('Pygame pythonspot.com example')
-        self._running = True
-        self._image_surf = pygame.image.load("pygame.png").convert()
-        self._apple_surf = pygame.image.load("apple.png").convert()
+        self.surface = pygame.display.set_mode((WIDTH * STEP, HEIGHT * STEP))
+        pygame.display.set_caption('Snake')
+        self.running = True
 
     def on_event(self, event):
         if event.type == QUIT:
-            self._running = False
+            self.running = False
 
     def on_loop(self):
-        self.computer.target(self.apple.x, self.apple.y)
-        self.player.update()
-        self.computer.update()
+        self.snake.move()
+        # TODO: Move this logic to snake
+        if self.snake.segments[0].x == self.apple.x and self.snake.segments[0].y == self.apple.y:
+            self.apple.place()
+            self.snake.grow()
+            if not (0 <= snake.segments[0].x < WIDTH and 0 <= snake.segments[0].y < HEIGHT):
 
-        # does snake eat apple?
-        for i in range(0,self.player.length):
-            if self.game.isCollision(self.apple.x,self.apple.y,self.player.x[i], self.player.y[i],44):
-                self.apple.x = randint(2,9) * 44
-                self.apple.y = randint(2,9) * 44
-                self.player.length = self.player.length + 1
-
-        # does computer eat apple?
-        for i in range(0,self.player.length):
-            if self.game.isCollision(self.apple.x,self.apple.y,self.computer.x[i], self.computer.y[i],44):
-                self.apple.x = randint(2,9) * 44
-                self.apple.y = randint(2,9) * 44
-                #self.computer.length = self.computer.length + 1
-
-
-        # does snake collide with itself?
-        for i in range(2,self.player.length):
-            if self.game.isCollision(self.player.x[0],self.player.y[0],self.player.x[i], self.player.y[i],40):
-                print("You lose! Collision: ")
-                print("x[0] (" + str(self.player.x[0]) + "," + str(self.player.y[0]) + ")")
-                print("x[" + str(i) + "] (" + str(self.player.x[i]) + "," + str(self.player.y[i]) + ")")
-                exit(0)
-
-        pass
 
     def on_render(self):
         self._display_surf.fill((0,0,0))
@@ -215,9 +101,9 @@ class App:
 
     def on_execute(self):
         if self.on_init() == False:
-            self._running = False
+            self.running = False
 
-        while( self._running ):
+        while( self.running ):
             pygame.event.pump()
             keys = pygame.key.get_pressed()
 
@@ -234,7 +120,7 @@ class App:
                 self.player.moveDown()
 
             if (keys[K_ESCAPE]):
-                self._running = False
+                self.running = False
 
             self.on_loop()
             self.on_render()
