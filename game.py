cb3c6122a908d419f38c02a6857659445f131c48
ErikBoesen <me@erikboesen.com>
2018-12-03 09:35:36 -0500

    Fix snake being able to go beyond bottom of screen

--- game.py
+++ game.py
@@ -52,7 +52,7 @@ class Snake:
         return False
 
     def outbound(self) -> bool:
-        return not (0 <= self.segments[0].x < WIDTH and 0 <= self.segments[0].y < HEIGHT)
+        return not (0 <= self.segments[0].x < WIDTH and 1 <= self.segments[0].y < HEIGHT)
 
     def ate(self, apple: Apple) -> bool:
         return self.segments[0].x == apple.x and self.segments[0].y == apple.y
